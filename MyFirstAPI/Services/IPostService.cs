de095b38a31053bc5afc1badee68acfe845bad28
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-08-15 21:39:59 +0300

    Define IPostService for posts to use with DI

---
+++ MyFirstAPI/Services/IPostService.cs
@@ -0,0 +1,17 @@
+﻿using MyFirstAPI.Models;
+
+namespace MyFirstAPI.Services
+{
+    public interface IPostService
+    {
+        public Task CreatePost(Post item);
+
+        public Task<Post?> UpdatePost(int id, Post item);
+
+        public Task<Post?> GetPost(int id);
+
+        public Task<List<Post>> GetAllPosts();
+
+        public Task DeletePost(int id);
+    }
+}
