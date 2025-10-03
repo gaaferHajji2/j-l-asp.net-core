f2a52efbdfabffc6d407524699f32ae9e36b1722
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-03 17:11:27 +0300

    Read db configuration

---
+++ EnvExample/Controllers/ConfController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnvExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfController(IConfiguration configuration) => _configuration = configuration;
+
+        [HttpGet]
+        [Route("test-01")]
+        public ActionResult GetConf()
+        {
+            var type = _configuration["Database:Type"];
+            var connStr = _configuration["Database:ConnectionString"];
+
+            return Ok(new {
+                Type= type, ConnStr= connStr 
+            });
+        }
+    }
+}
