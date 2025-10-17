39d889bdc7394f232ae9d68ab2e0aa5770f14a2e
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-18 01:51:34 +0300

    Build simple middleware app

---
+++ JLokaTestMiddleware/Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+     //await context.Response.WriteAsync("Hello JLoka Simple Middleware World");
+
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+    logger.LogInformation($"Request Host: {context.Request.Host}");
+    logger.LogInformation("My Middleware - Before");
+    await next(context);
+    logger.LogInformation("My Middleware - After");
+    logger.LogInformation($"Response StatusCode: {context.Response.StatusCode}");
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
