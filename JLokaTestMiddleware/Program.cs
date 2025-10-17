5917eef4b64d92aca3844e968ee232cd69ca2ac0
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-18 01:56:47 +0300

    Test change status code

--- JLokaTestMiddleware/Program.cs
+++ JLokaTestMiddleware/Program.cs
@@ -22,6 +22,17 @@ app.Use(async (context, next) =>
     logger.LogInformation($"Response StatusCode: {context.Response.StatusCode}");
 });
 
+app.Use(async (context, next) =>
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogInformation($"ClientName HttpHeader in Middleware 2: {context.Request.Headers["ClientName"]}");
+    logger.LogInformation("My Middleware 2 - Before");
+    context.Response.StatusCode = StatusCodes.Status202Accepted;
+    await next(context);
+    logger.LogInformation("My Middleware 2 - After");
+    logger.LogInformation($"Response StatusCode in Middleware 2: {context.Response.StatusCode}");
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
