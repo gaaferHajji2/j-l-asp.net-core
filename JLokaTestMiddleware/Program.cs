c31634bd28bc978d4ae3783269ce8e6d4064823c
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-06 04:48:30 +0300

    Adding correlation id middleware and extension

--- JLokaTestMiddleware/Program.cs
+++ JLokaTestMiddleware/Program.cs
@@ -29,31 +29,32 @@ builder.Services.AddRequestTimeouts(options =>
 var app = builder.Build();
 
 app.UseRateLimiter();
-app.UseRequestTimeouts();   
+app.UseRequestTimeouts();
+app.UseCorrelationId();
 
-app.Use(async (context, next) =>
-{
-     //await context.Response.WriteAsync("Hello JLoka Simple Middleware World");
+//app.Use(async (context, next) =>
+//{
+//     //await context.Response.WriteAsync("Hello JLoka Simple Middleware World");
 
-    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+//    var logger = app.Services.GetRequiredService<ILogger<Program>>();
 
-    logger.LogInformation($"Request Host: {context.Request.Host}");
-    logger.LogInformation("My Middleware - Before");
-    await next(context);
-    logger.LogInformation("My Middleware - After");
-    logger.LogInformation($"Response StatusCode: {context.Response.StatusCode}");
-});
+//    logger.LogInformation($"Request Host: {context.Request.Host}");
+//    logger.LogInformation("My Middleware - Before");
+//    await next(context);
+//    logger.LogInformation("My Middleware - After");
+//    logger.LogInformation($"Response StatusCode: {context.Response.StatusCode}");
+//});
 
-app.Use(async (context, next) =>
-{
-    var logger = app.Services.GetRequiredService<ILogger<Program>>();
-    logger.LogInformation($"ClientName HttpHeader in Middleware 2: {context.Request.Headers["ClientName"]}");
-    logger.LogInformation("My Middleware 2 - Before");
-    context.Response.StatusCode = StatusCodes.Status202Accepted;
-    await next(context);
-    logger.LogInformation("My Middleware 2 - After");
-    logger.LogInformation($"Response StatusCode in Middleware 2: {context.Response.StatusCode}");
-});
+//app.Use(async (context, next) =>
+//{
+//    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+//    logger.LogInformation($"ClientName HttpHeader in Middleware 2: {context.Request.Headers["ClientName"]}");
+//    logger.LogInformation("My Middleware 2 - Before");
+//    context.Response.StatusCode = StatusCodes.Status202Accepted;
+//    await next(context);
+//    logger.LogInformation("My Middleware 2 - After");
+//    logger.LogInformation($"Response StatusCode in Middleware 2: {context.Response.StatusCode}");
+//});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ JLokaTestMiddleware/middleware/CorrelationIdMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using JLokaTestMiddleware.middleware;
+
+namespace JLokaTestMiddleware.middleware
+{
+    public class CorrelationIdMiddleware(RequestDelegate next, ILogger<CorrelationIdMiddleware> logger)
+    {
+        private const string CorrelationIdHeaderName = "X-Correlation-Id";
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var correlationId = context.Request.Headers[CorrelationIdHeaderName].FirstOrDefault();
+            if (correlationId == null)
+            {
+                correlationId = Guid.NewGuid().ToString();
+            }
+
+            context.Request.Headers.TryAdd(CorrelationIdHeaderName, correlationId);
+            //log the data
+            logger.LogInformation($"Request Path: {context.Request.Path}, with correlation id: {correlationId}");
+            context.Response.Headers.TryAdd(CorrelationIdHeaderName, correlationId);
+            await next(context);
+        }
+    }
+}
+
+public static class CorrelationIdMiddlewareExtensions
+{
+    public static IApplicationBuilder UseCorrelationId(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<CorrelationIdMiddleware>();
+    }
+}
