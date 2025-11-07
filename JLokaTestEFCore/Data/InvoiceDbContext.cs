4f3661f3e2b4f6e0fc5d3084b2cb9a944d0120e2
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-07 05:34:16 +0300

    Generate models && migrations

---
+++ JLokaTestEFCore/Data/InvoiceDbContext.cs
@@ -0,0 +1,10 @@
+﻿using JLokaTestEFCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JLokaTestEFCore.Data
+{
+    public class InvoiceDbContext(DbContextOptions<InvoiceDbContext> options): DbContext(options)
+    {
+        public DbSet<Invoice> Invoices => Set<Invoice>();
+    }
+}
--- JLokaTestEFCore/Models/Invoice.cs
+++ JLokaTestEFCore/Models/Invoice.cs
@@ -1,6 +1,24 @@
-﻿namespace JLokaTestEFCore.Models
+﻿using JLokaTestEFCore.enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace JLokaTestEFCore.Models
 {
     public class Invoice
     {
+        public Guid Id { get; set; }
+        public string InvoiceNumber { get; set; } = string.Empty;
+
+        public string ContactName {  get; set; } = string.Empty;
+
+        public string? Description {  get; set; }
+
+        [Precision(18, 2)]
+        public decimal Amount { get; set; }
+
+        public DateTimeOffset InvoiceDate { get; set; }
+
+        public DateTimeOffset DueDate { get; set; }
+
+        public InvoiceStatus Status { get; set; }
     }
 }
