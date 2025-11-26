d46e0e69aeb0d64a3039d6945f16456c9b871711
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-26 12:08:32 +0300

    Using extensions for tables with Fluent API & generate migration file

---
+++ JLokaTestEFCore/Migrations/20251126085529_ConfigInvoiceModel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JLokaTestEFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigInvoiceModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Invoices",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InvoiceNumber",
+                table: "Invoices",
+                type: "varchar(32)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Invoices",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContactName",
+                table: "Invoices",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Invoices",
+                keyColumn: "Id",
+                keyValue: new Guid("3212976a-1f5a-462b-b989-17a80f9f6e79"),
+                column: "Status",
+                value: "AwaitPayment");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Invoices",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldMaxLength: 16);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InvoiceNumber",
+                table: "Invoices",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(32)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Invoices",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContactName",
+                table: "Invoices",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(32)",
+                oldMaxLength: 32);
+
+            migrationBuilder.UpdateData(
+                table: "Invoices",
+                keyColumn: "Id",
+                keyValue: new Guid("3212976a-1f5a-462b-b989-17a80f9f6e79"),
+                column: "Status",
+                value: 1);
+        }
+    }
+}
