2f8ec5b05810b78af75b82570359362c15522b7c
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-02 15:27:30 +0300

    Adding contacts controller

---
+++ JLokaTestEFCore/Controllers/JLokaContactsController.cs
@@ -0,0 +1,103 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JLokaTestEFCore.Data;
+using JLokaTestEFCore.Models;
+
+namespace JLokaTestEFCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JLokaContactsController : ControllerBase
+    {
+        private readonly InvoiceDbContext _context;
+
+        public JLokaContactsController(InvoiceDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/JLokaContacts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> GetContacts()
+        {
+            return await _context.Contacts.ToListAsync();
+        }
+
+        // GET: api/JLokaContacts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contact>> GetContact(Guid id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return contact;
+        }
+
+        // PUT: api/JLokaContacts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContact(Guid id, Contact contact)
+        {
+            if (id != contact.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(contact).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/JLokaContacts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Contact>> PostContact(Contact contact)
+        {
+            _context.Contacts.Add(contact);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetContact", new { id = contact.Id }, contact);
+        }
+
+        // DELETE: api/JLokaContacts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContact(Guid id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contacts.Remove(contact);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ContactExists(Guid id)
+        {
+            return _context.Contacts.Any(e => e.Id == id);
+        }
+    }
+}
