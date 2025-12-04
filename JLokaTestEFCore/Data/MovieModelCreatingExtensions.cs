8e05fe6286929e5596a494a65824df451481902d
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-04 15:29:41 +0300

    Adding MovieActor && migrations && configurations

--- JLokaTestEFCore/Data/MovieModelCreatingExtensions.cs
+++ JLokaTestEFCore/Data/MovieModelCreatingExtensions.cs
@@ -16,6 +16,16 @@ namespace JLokaTestEFCore.Data
                 b.Property(p => p.Title).HasColumnName(nameof(Movie.Title)).HasMaxLength(255).IsRequired();
                 b.Property(p => p.Description).HasColumnName(nameof(Movie.Description));
                 b.Property(p => p.ReleaseYear).HasColumnName(nameof(Movie.ReleaseYear)).IsRequired();
+                b.HasMany(m => m.Actors).WithMany(a => a.Movies).UsingEntity<MovieActor>(
+                    j => j.HasOne(ma => ma.Actor).WithMany(a => a.MovieActors).HasForeignKey(ma => ma.ActorId),
+                    j => j.HasOne(ma => ma.Movie).WithMany(m => m.MovieActors).HasForeignKey(ma => ma.MovieId),
+                    j =>
+                    {
+                        j.Property(ma => ma.UpdateTime).HasColumnName(nameof(MovieActor.UpdateTime))
+                            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+                        j.HasKey(ma => new { ma.MovieId, ma.ActorId});
+                    }
+                );
             });
 
         }
---
+++ JLokaTestEFCore/Migrations/20251204122153_AddMovieAndActor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JLokaTestEFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMovieAndActor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReleaseYear = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MovieActor",
+                columns: table => new
+                {
+                    MovieId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ActorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UpdateTime = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieActor", x => new { x.MovieId, x.ActorId });
+                    table.ForeignKey(
+                        name: "FK_MovieActor_Actors_ActorId",
+                        column: x => x.ActorId,
+                        principalTable: "Actors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MovieActor_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieActor_ActorId",
+                table: "MovieActor",
+                column: "ActorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovieActor");
+
+            migrationBuilder.DropTable(
+                name: "Actors");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
