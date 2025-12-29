162f616e26f1e8af24bce6dc6d053bf72b3c5d49
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-29 10:45:54 +0300

    Adding login user model

--- JLokaAuthentication/Authentication/AddOrUpdateAppUserModel.cs
+++ JLokaAuthentication/Authentication/AddOrUpdateAppUserModel.cs
@@ -5,12 +5,12 @@ namespace JLokaAuthentication.Authentication
     public class AddOrUpdateAppUserModel
     {
         [Required(ErrorMessage = "Username is required")]
-        public String UserName { get; set; } = String.Empty;
+        public string UserName { get; set; } = String.Empty;
         [Required(ErrorMessage = "Password is required")]
-        public String Password { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
         [EmailAddress]
         [Required(ErrorMessage ="Email is Required")]
-        public String Email { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
 
     }
 }
---
+++ JLokaAuthentication/Authentication/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JLokaAuthentication.Authentication
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; } = String.Empty;
+        [Required(ErrorMessage = "Password is required")]
+        public string password { get; set; } = string.Empty;
+    }
+}
