b87b9a8ae7470b0ddf4e2818056d153972bb2594
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-29 12:45:14 +0300

    Remove unused code & adding migrations with options to use JWT

--- JLokaAuthentication/Authentication/AppUser.cs
+++ JLokaAuthentication/Authentication/AppUser.cs
@@ -4,5 +4,10 @@ namespace JLokaAuthentication.Authentication
 {
     public class AppUser: IdentityUser
     {
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string ProfilePicture {  get; set; } = string.Empty;
     }
 }
--- JLokaAuthentication/Program.cs
+++ JLokaAuthentication/Program.cs
@@ -1,25 +1,74 @@
-var builder = WebApplication.CreateBuilder(args);
+using JLokaAuthentication.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+
+        // Add the db context
+        builder.Services.AddDbContext<AppDbContext>();
+        // Add the identity model and attach to db context
+        builder.Services.AddIdentityCore<AppUser>()
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+        //Add authentication with options
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            var secret = builder.Configuration["JwtConfig:secret"];
+            var issuer = builder.Configuration["JwtConfig:ValidIssuer"];
+            var audience = builder.Configuration["JwtConfig:ValidAudiences"];
+
+            if (secret is null || issuer is null || audience is null)
+            {
+                throw new ApplicationException("Jwt is not set in the configuration");
+            }
+
+            options.SaveToken = true;
+            options.RequireHttpsMetadata = false;
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = audience,
+                ValidIssuer = issuer,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
+            };
+        });
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
