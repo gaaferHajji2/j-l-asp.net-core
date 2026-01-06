e5b8ba8a935f3f3ba879a127c7cf9645335cb329
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-01-06 13:00:11 +0300

    Adding generate token

--- JLokaAuthentication/Controllers/AccountController.cs
+++ JLokaAuthentication/Controllers/AccountController.cs
@@ -1,12 +1,16 @@
 ﻿using JLokaAuthentication.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace JLokaAuthentication.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class AccountController(UserManager<AppUser> userManager, IConfiguration configuration) : ControllerBase
+    public class AccountController(UserManager<AppUser> userManager, IConfiguration _configuration) : ControllerBase
     {
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] AddOrUpdateAppUserModel model)
@@ -50,9 +54,38 @@ namespace JLokaAuthentication.Controllers
             return BadRequest(ModelState);
         }
 
-        private object GenerateToken(string userName)
+        private string? GenerateToken(string userName)
         {
-            throw new NotImplementedException();
+            var secret = _configuration["JwtConfig:Secret"];
+            var issuer = _configuration["JwtConfig:ValidIssuer"];
+            var audience = _configuration["JwtConfig:ValidAudiences"];
+
+            if(secret is null || issuer is null || audience is null)
+            {
+                throw new ApplicationException("Jwt is not set in the configuration");
+            }
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, userName),
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                Issuer = issuer,
+                Audience = audience,
+                SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256Signature),
+            };
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.WriteToken(securityToken);
+            return token;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+
         }
     }
 }
