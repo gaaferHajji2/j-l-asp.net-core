25b99cc45c2b0ea1fd64ade00bb78c2f99a4a978
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-20 11:44:12 +0300

    Init db fixture

---
+++ InvoiceAppTesting/JLoka.InvoiceApp.UnitTests/TestDatabaseFixture.cs
@@ -0,0 +1,12 @@
+﻿using InvoiceApp.WebApi.Data;
+using Microsoft.EntityFrameworkCore;
+namespace JLoka.InvoiceApp.UnitTests
+{
+    internal class TestDatabaseFixture
+    {
+        private const string ConnectionString = @"Server=DESKTOP-3UNEJDA\\SQLEXPRESS;Database=JLokaDB_02;
+        Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true";
+
+        public InvoiceDbContext CreateDbContext() => new(new DbContextOptionsBuilder<InvoiceDbContext>().UseSqlServer(ConnectionString).Options, null);
+    }
+}
