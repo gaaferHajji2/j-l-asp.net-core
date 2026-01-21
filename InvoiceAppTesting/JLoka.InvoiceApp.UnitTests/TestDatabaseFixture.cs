f4a432daa7db30db361398a469653fbd94292a52
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-21 08:58:42 +0300

    Adding db fixture

--- InvoiceAppTesting/JLoka.InvoiceApp.UnitTests/TestDatabaseFixture.cs
+++ InvoiceAppTesting/JLoka.InvoiceApp.UnitTests/TestDatabaseFixture.cs
@@ -7,7 +7,32 @@ namespace JLoka.InvoiceApp.UnitTests
         private const string ConnectionString = @"Server=DESKTOP-3UNEJDA\\SQLEXPRESS;Database=JLokaDB_02;
         Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true";
 
+        private static readonly object Lock = new();
+        private static bool _databaseInitialized;
+
+        public TestDatabaseFixture()
+        {
+            // This code comes from Microsoft Docs: https://github.com/dotnet/EntityFramework.Docs/blob/main/samples/core/Testing/TestingWithTheDatabase/TestDatabaseFixture.cs
+            lock (Lock)
+            {
+                if (!_databaseInitialized!)
+                {
+                    InitializeDatabase();
+                    _databaseInitialized = true;
+                }
+            }
+        }
+
+
         public InvoiceDbContext CreateDbContext() => new(new DbContextOptionsBuilder<InvoiceDbContext>().UseSqlServer(ConnectionString).Options, null);
 
+        public void InitializeDatabase()
+        {
+            using var context = CreateDbContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            context.Database.Migrate();
+        }
     }
 }
