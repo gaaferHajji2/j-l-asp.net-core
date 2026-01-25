ef85d6444f15a8c9fd9fc81e410eefbb5a6936f8
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-25 14:09:41 +0300

    Adding contact grpc service

---
+++ JLokaGrpcService/Services/ContactGrpcService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using JLokaGrpcService.Protos;
+
+namespace JLokaGrpcService.Services
+{
+    public class ContactGrpcService(ILogger<ContactGrpcService> logger): ContactService.ContactServiceBase
+    {
+        public override Task<CreateContactResponse> CreateContact(CreateContactRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new CreateContactResponse
+            {
+                ContactId = Guid.NewGuid().ToString(),
+            });
+        }
+    }
+}
