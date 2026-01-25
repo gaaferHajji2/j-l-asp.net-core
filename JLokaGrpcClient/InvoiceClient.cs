b45c06af6c86a55b33bc7a85c8149e494b155c63
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-25 14:35:59 +0300

    Fix channel url issue

---
+++ JLokaGrpcClient/InvoiceClient.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+using JLokaGrpcService.Protos;
+
+namespace JLokaGrpcClient
+{
+    internal class InvoiceClient
+    {
+        public async Task CreateContactAsync()
+        {
+            using var channel = GrpcChannel.ForAddress("http://localhost:5077");
+            var client = new ContactService.ContactServiceClient(channel);
+            var reply = await client.CreateContactAsync(new CreateContactRequest
+            {
+                Email = "john.doe@abc.com",
+                FirstName = "John",
+                LastName = "Doe",
+                IsActive = true,
+                Phone = "1234567890",
+                YearOfBirth = 1980
+            });
+
+            Console.WriteLine("Created Contact: " + reply.ContactId);
+            Console.ReadKey();
+        }
+    }
+}
--- JLokaGrpcClient/Program.cs
+++ JLokaGrpcClient/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using JLokaGrpcClient;
+
 Console.WriteLine("Hello, World!");
+var contactClient = new InvoiceClient();
+await contactClient.CreateContactAsync();
