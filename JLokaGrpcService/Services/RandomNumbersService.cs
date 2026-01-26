d8e8dfbeb511020c83def80bcc3556ed789a7665
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-26 10:25:50 +0300

    Adding random number service for server

---
+++ JLokaGrpcService/Services/RandomNumbersService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using JLokaGrpcService.Protos;
+
+namespace JLokaGrpcService.Services
+{
+    public class RandomNumbersService(ILogger<RandomNumbersService> _logger): RandomNumbers.RandomNumbersBase
+    {
+        public override async Task GetRandomNumbers(GetRandomNumbersRequest request, IServerStreamWriter<GetRandomNumbersResponse> responseStream, 
+            ServerCallContext context)
+        {
+            var random = new Random();
+            for (var i = 0; i < request.Count; i++)
+            {
+                await responseStream.WriteAsync(new GetRandomNumbersResponse
+                {
+                    Number = random.Next(request.Min, request.Max)
+                });
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
