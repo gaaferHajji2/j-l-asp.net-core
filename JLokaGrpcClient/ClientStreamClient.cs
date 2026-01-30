d5ada638b034308969beff06305a65ecbb8b7786
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-30 09:07:50 +0300

    Adding client streaming client

---
+++ JLokaGrpcClient/ClientStreamClient.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Net.Client;
+using JLokaGrpcService.Protos;
+
+namespace JLokaGrpcClient
+{
+    internal class ClientStreamClient
+    {
+        public async Task SendClientRandomNumbers()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:7240");
+            var client = new RandomNumbersClient.RandomNumbersClientClient(channel);
+            //create the streaming request
+            using var clientStreamingCall = client.SendRandomNumbersClient();
+            var random = new Random();
+            for(var i=0; i<5; ++i)
+            {
+                await clientStreamingCall.RequestStream.WriteAsync(new SendRandomNumbersRequest
+                {
+                    Number = random.Next(1, 100)
+                });
+                await Task.Delay(100);
+            }
+
+            await clientStreamingCall.RequestStream.CompleteAsync();
+
+            // Get the response
+            var response = await clientStreamingCall;
+            Console.WriteLine($"Count: {response.Count}, Sum: {response.Sum}");
+            Console.ReadKey();
+        }
+    }
+}
