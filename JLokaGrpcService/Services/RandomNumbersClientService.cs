dadf4693a71d5af3d115d8047ddc207d3e652a77
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-30 08:53:20 +0300

    Adding client streaming service

---
+++ JLokaGrpcService/Services/RandomNumbersClientService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using JLokaGrpcService.Protos;
+
+namespace JLokaGrpcService.Services
+{
+    public class RandomNumbersClientService(ILogger<RandomNumbersClientService> logger) : RandomNumbersClient.RandomNumbersClientBase
+    {
+        public override async Task<SendRandomNumbersResponse> SendRandomNumbersClient(IAsyncStreamReader<SendRandomNumbersRequest> requestStream, ServerCallContext context)
+        {
+            var count = 0;
+            var sum = 0;
+            await foreach (var request in requestStream.ReadAllAsync())
+            {
+                logger.LogInformation($"Received: {request.Number}");
+                ++count;
+                sum += request.Number;
+            }
+            return new SendRandomNumbersResponse
+            {
+                Sum = sum,
+                Count = count
+            };
+        }
+    }
+}
