64a7a3fbfdb47e01c95269e46fb2db09b9cf791b
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-31 08:42:01 +0300

    Adding chat service

---
+++ JLokaGrpcService/Services/ChatService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+using JLokaGrpcService.Protos;
+
+namespace JLokaGrpcService.Services
+{
+    public class ChatService(ILogger<ChatService> logger): Chat.ChatBase
+    {
+        public override async Task SendMessage(IAsyncStreamReader<ChatMessage> requestStream, IServerStreamWriter<ChatMessage> responseStream,
+            ServerCallContext context)
+        {
+            await foreach (var request in requestStream.ReadAllAsync())
+            {
+                logger.LogInformation($"Received: {request.Message}");
+                await responseStream.WriteAsync(new ChatMessage
+                {
+                    Message = $"You said: {request.Message.ToUpper()}",
+                });
+            }
+        }
+    }
+}
