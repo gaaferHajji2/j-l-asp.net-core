1b83169d25997892ce7f9acdceffa867f2725ef6
JafarLoka <gaafer.hajji1995@gmail.com>
2026-02-01 09:20:58 +0300

    Build simple client api for grpc service

---
+++ GrpcClient02/ContactClient.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Net.Client;
+using GrpcDemo;
+
+namespace GrpcClient02
+{
+    internal class ContactClient
+    {
+        public async Task SendRequest()
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:7214");
+            var client = new Contact.ContactClient(channel);
+        }
+    }
+}
---
+++ JLokaGrpc.API/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using GrpcDemo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JLokaGrpc.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController (Contact.ContactClient client, ILogger<ContactController> logger) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateContact(CreateContactRequest request)
+        {
+            var replay = await client.CreateContactAsync(request);
+            logger.LogInformation($"Replay is: {replay.ContactId}");
+            return Ok(replay);
+        }
+    }
+}
