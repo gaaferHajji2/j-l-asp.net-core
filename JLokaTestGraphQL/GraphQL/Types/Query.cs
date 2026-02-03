3d72a63022c208d98a1c76fc4c055a93b86bd4a5
JafarLoka <gaafer.hajji1995@gmail.com>
2026-02-03 09:17:17 +0300

    Adding query for teacher & department

--- JLokaTestGraphQL/GraphQL/Types/Query.cs
+++ JLokaTestGraphQL/GraphQL/Types/Query.cs
@@ -6,6 +6,6 @@ namespace JLokaTestGraphQL.GraphQL.Types;
 
 public class Query
 {
-    public async Task<List<Teacher>> GetTeachers([Service] AppDbContext context) => await context.Teachers.ToListAsync();
+    public async Task<List<Teacher>> GetTeachers([Service] AppDbContext context) => await context.Teachers.Include(x => x.Department).ToListAsync();
     public async Task<Teacher?> GetTeacher(Guid id, [Service] AppDbContext context) => await context.Teachers.FindAsync(id);
 }
