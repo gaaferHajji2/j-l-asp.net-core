42abdfce69bcc02c395340a874bb3f6fbb4cd166
JafarLoka <gaafer.hajji1995@gmail.com>
2026-02-08 14:53:53 +0300

    Adding TeacherResolver & use ResolveWith

--- JLokaTestGraphQL/GraphQL/Types/TeacherType.cs
+++ JLokaTestGraphQL/GraphQL/Types/TeacherType.cs
@@ -1,5 +1,6 @@
 ﻿using JLokaTestGraphQL.Data;
 using JLokaTestGraphQL.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace JLokaTestGraphQL.GraphQL.Types;
 
@@ -9,11 +10,22 @@ public class TeacherType: ObjectType<Teacher>
     {
         Console.WriteLine("Configure the TeacherType");
 
-        descriptor.Field(x => x.Department).Name("department").Description("Simple Description").Resolve(async context =>
-        {
-            var department = await context.Service<AppDbContext>().Departments.FindAsync(context.Parent<Teacher>().DepartmentId);
-            Console.WriteLine("Loading Department");
-            return department;
-        });
+        //descriptor.Field(x => x.Department).Name("department").Description("Simple Description").Resolve(async context =>
+        //{
+        //    var department = await context.Service<AppDbContext>().Departments.FindAsync(context.Parent<Teacher>().DepartmentId);
+        //    Console.WriteLine("Loading Department");
+        //    return department;
+        //});
+        descriptor.Field(x => x.Department).Description("Simple Department Resolver").ResolveWith<TeacherResolvers>(x => x.GetDepartment(default, default));
+    }
+}
+
+public class TeacherResolvers
+{
+    public async Task<Department> GetDepartment([Parent] Teacher teacher, [Service] IDbContextFactory<AppDbContext> dbContextFactory)
+    {
+        await using var dbContext = await dbContextFactory.CreateDbContextAsync();
+        var department = await dbContext.Departments.FindAsync(teacher.DepartmentId);
+        return department;
     }
 }
