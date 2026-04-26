d12666992ab92920eb7f5890cba25c0824f04fc6
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-26 09:36:41 +0300

    Use collection expression

--- JLokaAuthentication/Controllers/WeatherForecastController.cs
+++ JLokaAuthentication/Controllers/WeatherForecastController.cs
@@ -11,10 +11,10 @@ namespace JLokaAuthentication.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
+        private static readonly string[] Summaries =
+        [
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        ];
 
         private readonly ILogger<WeatherForecastController> _logger;
 
