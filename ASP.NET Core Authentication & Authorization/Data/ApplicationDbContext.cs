0b99934ed76d5201db65ed10bf51b21c8167459b
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-10 07:47:54 +0300

    Create ApplicationUser & Application Db Context

---
+++ ASP.NET Core Authentication & Authorization/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_Core_Authentication___Authorization.Data
+{
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ ASP.NET Core Authentication & Authorization/Data/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ASP.NET_Core_Authentication___Authorization.Data
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime? LastLogin { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
