2a1ea3bf9fafcd59042e6ba4fb0c4a12b8ba05d6
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-24 15:50:10 +0300

    Adding UserConnection class

---
+++ JLoka.ChatService/Models/UserCollection.cs
@@ -0,0 +1,7 @@
+﻿namespace JLoka.ChatService.Models;
+
+public class UserCollection
+{
+    public string Username { get; set; } = string.Empty;
+    public string ChatRoom { get; set; } = string.Empty;
+}
