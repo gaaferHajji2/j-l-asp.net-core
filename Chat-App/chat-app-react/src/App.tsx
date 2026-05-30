0d88b2ac7886d7d4230de385eb1d6f163bc5c8c1
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-30 14:03:56 +0300

    Adding the connection to react app

--- Chat-App/chat-app-react/src/App.tsx
+++ Chat-App/chat-app-react/src/App.tsx
@@ -1,16 +1,41 @@
+import { useState } from 'react';
 import './App.css'
-import WaitingRoom from './components/WaitingRoom';
+import WaitingRoom from './components/WaitingRoom'
+import { HubConnection, HubConnectionBuilder, LogLevel} from '@microsoft/signalr'
 
 function App() {
+
+  const [connection, setConnection] = useState<HubConnection>()
+  const joinChatRoom = async (username: string, chatRoom: string) => {
+    try {
+      const conn = new HubConnectionBuilder()
+        .withUrl("https://localhost:7260/Chat")
+        .configureLogging(LogLevel.Information)
+        .build()
+
+      conn.on("JoinSpecificChatRoom", (username: string, msg: string) => {
+        console.log("The username is: ", username, ", and the msg is: ", msg)
+      })
+
+      conn.on("ReceiveMessage", (username: string, msg: string) => {
+        console.log("The username of receive msg is: ", username, ", and the msg is: ", msg)
+      })
+
+      await conn.start()
+      await conn.invoke("JoinSpecificChatRoom", {username, chatRoom})
+      setConnection(conn)
+    } catch (e) {
+      console.log("Error during the connection: ", e)
+    }
+  }
+
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="px-5 my-5 w-full">
         <h1 className="text-4xl font-light">Welcome to JLoka Chat App</h1>
       </div>
 
-      <WaitingRoom joinChatRoom={(username: string, chatRoom:string) => {
-        console.log(`The username is: ${username} & chatRoom is: ${chatRoom}`)
-      }}/>
+      <WaitingRoom joinChatRoom={joinChatRoom}/>
     </div>
   );
 }
