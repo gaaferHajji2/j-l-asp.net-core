b2cf193ec1f2749f577a4a246b05c8bf9672ab78
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-31 15:02:54 +0300

    Use shared with connections to store the UserConnection Values

--- JLoka.ChatService/Hubs/ChatHub.cs
+++ JLoka.ChatService/Hubs/ChatHub.cs
@@ -19,6 +19,15 @@ public class ChatHub: Hub
     public async Task JoinSpecificChatRoom(UserConnection conn)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, conn.ChatRoom);
+        _shared.connections[Context.ConnectionId] = conn;
         await Clients.Group(conn.ChatRoom).SendAsync("JoinSpecificChatRoom", "admin", $"{conn.Username} has joined {conn.ChatRoom}");
+    }
+
+    public async Task SendMessage(string msg)
+    {
+        if(_shared.connections.TryGetValue(Context.ConnectionId, out UserConnection conn))
+        {
+            await Clients.Group(conn.ChatRoom).SendAsync("ReceiveSpecificMessage", conn.Username, msg);
+        }
     }
 }
